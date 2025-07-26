44ef5c5c1d7ca8c25d16725d8cfe3bf89a689a92
Hải Long <longdh2.dev@gmail.com>
2025-07-26 09:26:27 +0700

    feat: add Notification

---
+++ internal/model/notifiaction.model.js
@@ -0,0 +1,37 @@
+import { Schema, model } from 'mongoose';
+
+const Documents = "Notification";
+const Collections = "Notifications";
+
+const notificationSchema = new Schema({
+    notif_user_receive: { 
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    notif_user_sender: { 
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    notif_message: { 
+        type: String, 
+        required: true
+    },
+    notif_type: {
+        type: String, 
+        enum: ["friend_request", "message"], 
+        required: true
+    },
+    notif_status: { 
+        type: String, 
+        enum: ["accepted", "rejected", "waiting", "read", "unread"], 
+        default: "waiting" 
+    },
+}, {
+    timestamps: true,
+    collection: Collections,
+});
+
+const notificationModel = model(Documents, notificationSchema);
+notificationSchema.index({ notif_user_receive: 1, notif_user_sender: 1, notif_type: 1 });
+
+export default notificationModel;
