54f2f7187464a48d2c079adaa7279f312f241932
LÊ THIÊN TRÍ <thientrile2003@gmail.com>
2025-07-27 09:37:15 +0700

    fix: Correct redis URL construction logic for authentication handling

--- internal/initializes/redis.init.js
+++ internal/initializes/redis.init.js
@@ -1,10 +1,13 @@
 import { Config } from "../../global.js";
 import { initRedis } from "../../pkg/redis/redis.js";
 const { user, pass, host } = Config.redis;
+
 const redisUser = encodeURIComponent(user);
 const redisPass = encodeURIComponent(pass);
-
-const redisUrl = `redis://${redisUser}:${redisPass}@${host}`;
+let redisUrl = `redis://${redisUser}:${redisPass}@${host}`;
+if(user && pass ) {
+ redisUrl = `redis://${host}`;
+}
 
 const InitRedis=  async ()  =>{
     global.RedisClient =await initRedis(redisUrl);
