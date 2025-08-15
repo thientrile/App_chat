f142393d6fddcec8ea465fa3d3e2ce0f59197144
Liguni <thientrile2003@gmail.com>
2025-08-15 09:46:43 +0700

    Revert "feat: Add MessageRouter for handling chat room requests and update do…"

--- internal/router/index.js
+++ internal/router/index.js
@@ -4,12 +4,11 @@ import express from 'express';
 import AccoutRouter from './Account/account.router.js';
 import { healthCheck } from '../controller/index.js';
 import ProfileRouter from './Profile/profile.router.js';
-import MessageRouter from './Message/message.router.js';
 
 const Router =express.Router();
 Router.get('/', asyncHandler(healthCheck))
 
 Router.use("/auth",AccoutRouter)
 Router.use("/profile",ProfileRouter)
-Router.use("/message",MessageRouter)
+
 export default Router;
